38997df846b296e8ab041ce20c9f671411de59e0
Koritato <rlaghwls249@gmail.com>
2026-04-23 17:43:28 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 
           <div class="input-container">
             <div class="input-grid">
-              <label for="height-slider">나의 신장 입력 (cm)</label>
+              <label for="height-slider">나의 신장 입력 (cm) zz</label>
               <div class="height-display">
                 <span> {{ currentHeight }} </span>cm
               </div>
